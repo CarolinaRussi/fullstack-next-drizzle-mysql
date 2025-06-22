15dca4055477e2c092a2b39378de067643fd81d3
Carolina Russi <carolinaferlarussi@gmail.com>
2025-06-22 17:35:33 -0300

    chore: atualizacao dos campos para ingles padronizado

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,16 @@
-import "dotenv/config";
-import { drizzle } from "drizzle-orm/mysql2";
+import { drizzle, MySql2DrizzleConfig } from "drizzle-orm/mysql2";
+import mysql from "mysql2/promise";
+import * as schema from "./schema";
 
-const db = drizzle({
-  connection: {
-    uri: process.env.DATABASE_URL,
-  },
+const pool = mysql.createPool({
+  uri: process.env.DATABASE_URL,
 });
+
+const config: MySql2DrizzleConfig<typeof schema> = {
+  schema,
+  mode: "default",
+};
+
+const db = drizzle(pool, config);
 
 export { db };
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -2,25 +2,27 @@ import { mysqlTable as table } from "drizzle-orm/mysql-core";
 import * as t from "drizzle-orm/mysql-core";
 import { AnyMySqlColumn } from "drizzle-orm/mysql-core";
 
-export const empresa = table("empresa", {
+export const company = table("company", {
   id: t.int("id").primaryKey().autoincrement(),
-  razaoSocial: t.varchar("razao_social", { length: 255 }).notNull(),
+  corporateName: t.varchar("corporate_name", { length: 255 }).notNull(),
   cnpj: t.varchar("cnpj", { length: 20 }).notNull(),
-  cep: t.varchar("cep", { length: 20 }).notNull(),
-  cidade: t.varchar("cidade", { length: 100 }).notNull(),
-  estado: t.varchar("estado", { length: 50 }).notNull(),
-  bairro: t.varchar("bairro", { length: 100 }).notNull(),
-  complemento: t.varchar("complemento", { length: 255 }).notNull(),
+  zipCode: t.varchar("zip_code", { length: 20 }).notNull(),
+  city: t.varchar("city", { length: 100 }).notNull(),
+  state: t.varchar("state", { length: 50 }).notNull(),
+  district: t.varchar("district", { length: 100 }).notNull(),
+  complement: t.varchar("complement", { length: 255 }).notNull(),
 });
 
-export const licenca = table("licenca", {
+export const license = table("license", {
   id: t.int("id").primaryKey().autoincrement(),
-  empresaId: t
-    .int("empresa_id")
+  companyId: t
+    .int("company_id")
     .notNull()
-    .references((): AnyMySqlColumn => empresa.id),
-  numero: t.varchar("numero", { length: 100 }).notNull(),
-  orgaoAmbiental: t.varchar("orgao_ambiental", { length: 255 }).notNull(),
-  emissao: t.date("emissao").notNull(),
-  validade: t.date("validade").notNull(),
+    .references((): AnyMySqlColumn => company.id),
+  number: t.varchar("number", { length: 100 }).notNull(),
+  environmentalAgency: t
+    .varchar("environmental_agency", { length: 255 })
+    .notNull(),
+  issuedAt: t.date("issued_at").notNull(),
+  expiresAt: t.date("expires_at").notNull(),
 });
