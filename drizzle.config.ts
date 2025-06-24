b441bbd8510d1743d2d8b95377ef2887ef254221
Carolina Russi <carolinaferlarussi@gmail.com>
2025-06-23 23:10:32 -0300

    chore: implementacao de tipo de conexao com o banco em producao e em modo de desenvolvimento

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,24 +1,23 @@
 import { defineConfig } from "drizzle-kit";
 
-//const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
   out: "drizzle",
   schema: "src/db/schema.ts",
   dialect: "mysql",
-  dbCredentials:
-    //isProduction?
-    {
-      host: process.env.DB_HOST!,
-      port: Number(process.env.DB_PORT!),
-      user: process.env.DB_USER!,
-      password: process.env.DB_PASSWORD!,
-      database: process.env.DB_NAME!,
-      ssl: {
-        rejectUnauthorized: false,
+  dbCredentials: isProduction
+    ? {
+        host: process.env.DB_HOST!,
+        port: Number(process.env.DB_PORT!),
+        user: process.env.DB_USER!,
+        password: process.env.DB_PASSWORD!,
+        database: process.env.DB_NAME!,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        url: process.env.DATABASE_URL!,
       },
-    },
-  //  : {
-  //      url: process.env.DATABASE_URL!,
-  //    },
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -2,23 +2,22 @@ import { drizzle, MySql2DrizzleConfig } from "drizzle-orm/mysql2";
 import mysql from "mysql2/promise";
 import * as schema from "./schema";
 
-//const isProduction = process.env.NODE_ENV === "production";
+const isProduction = process.env.NODE_ENV === "production";
 
-const pool =
-  //isProduction ?
-  mysql.createPool({
-    host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    ssl: {
-      rejectUnauthorized: false,
-    },
-  });
-// : mysql.createPool({
-//     uri: process.env.DATABASE_URL,
-//   });
+const pool = isProduction
+  ? mysql.createPool({
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: false,
+      },
+    })
+  : mysql.createPool({
+      uri: process.env.DATABASE_URL,
+    });
 
 const config: MySql2DrizzleConfig<typeof schema> = {
   schema,
